19aa334246df078f8e914bbb2af1efea8abb9e8b
Harsh Pratyush <Harsh.Pratyush@OAKNORTH.AI>
2020-10-21 14:13:13 +0530

    Added

--- src/main/java/org/oaknorth/graphql/server/resolvers/QueryResolver.java
+++ src/main/java/org/oaknorth/graphql/server/resolvers/QueryResolver.java
@@ -4,6 +4,7 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.oaknorth.graphql.server.entity.BankDetails;
 import org.oaknorth.graphql.server.exception.ValidationFailedException;
 import org.oaknorth.graphql.server.service.BankService;
+import org.oaknorth.graphql.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
@@ -16,11 +17,15 @@ public class QueryResolver implements GraphQLQueryResolver {
     @Autowired
     private BankService bankService;
 
+    @Autowired
+    private UserService userService;
+
     @PreAuthorize("hasAnyAuthority('USER','ADMIN','BANKER')")
     public List<BankDetails> allBank() {
             return bankService.bankDetailsList();
     }
 
+    @PreAuthorize("!isAnonymous()")
     public BankDetails bank(String accountNumber) {
             return bankService.bankDetails(accountNumber).orElseThrow(()-> new ValidationFailedException("Bank details Not Found"));
         }
